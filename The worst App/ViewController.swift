a4aa037a1277c109e0234387884ba5389a222a07
Van6242 <qhart52336@gmail.com>
2022-03-21 09:22:09 -0400

    Just a bad app

--- The worst App/ViewController.swift
+++ The worst App/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var timer = Timer()
+    
+  
+    @IBOutlet weak var selable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func badbutton(_ sender: Any) {
+        seisure()
+        selable.text = ("Why are you doing this to yourself")
+    }
+    func seisure() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(rescedualtimer), userInfo: nil, repeats: true)
+        rescedualtimer()
+    }
 
-
+    @objc func rescedualtimer() {
+        
+        if view.backgroundColor == UIColor.yellow
+        {
+            view.backgroundColor = UIColor.red
+            selable.textColor = UIColor.yellow
+        }
+        else
+        {
+            view.backgroundColor = UIColor.yellow
+            selable.textColor = UIColor.red
+        }
+        
+    }
+    
 }
 
